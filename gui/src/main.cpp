4c66c2c6207957ac0175987545131361d5fae14f
Hu-Tao128 <alcantarahuerta128@gmail.com>
2026-03-11 01:18:04 -0700

    fix(ci): resolve cross-platform CMake GUI linking issues

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
     engine.loadFromModule("MultiLink", "Main");
 #else
-    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/MultiLink/qml/Main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/MultiLink/Main.qml")));
 #endif
     return app.exec();
 }
